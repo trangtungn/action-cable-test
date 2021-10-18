b0001806154a4c2337128446206645e3068f57e7
Trang Tung Nguyen <trangtungn@gmail.com>
2021-10-19 00:41:21 +0700

    Update channel connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,26 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   helper_method :current_user, :logged_in?
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
-
   protected
 
   def authenticate_user!
     redirect_to root_path unless logged_in?
+  end
+
+  def current_user
+    @current_user ||= begin
+      return nil unless session[:user_id]
+
+      user = User.find(session[:user_id])
+      cookies.signed[:user_id] = user.id
+
+      user
+    end
+  end
+
+  private
+
+  def logged_in?
+    !!current_user
   end
 end
